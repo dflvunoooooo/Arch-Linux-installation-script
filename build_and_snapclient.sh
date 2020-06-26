e76bc6d76c0dfb75cfc67aefbade9cb8f0f7e788
dflvunoooooo <32816598+dflvunoooooo@users.noreply.github.com>
2020-06-26 18:07:35 +0200

    Update build_and_snapclient.sh

--- build_and_snapclient.sh
+++ build_and_snapclient.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+# install with curl -sL https://git.io/Jfhdr | bash
 sudo cp /usr/lib/systemd/system/snapclient.service /usr/lib/systemd/system/snapclient.service.update.bak
 sudo rm -r snapcast
 git clone https://github.com/badaix/snapcast.git 
 cd snapcast/externals 
 git submodule update --init --recursive 
 cd .. 
-cd lient 
+cd client 
 make
 sudo make install
 sudo rm /usr/lib/systemd/system/snapclient.service.pacnew
